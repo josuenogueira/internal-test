86bf37eb8cc3a45b14d098361a28d1c2aae89755
josuenogueira <josuenogueira@gmail.com>
2015-06-16 16:30:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
+  response.setHeader('content-type', 'application/json');
   response.send("{'export' : {'RETURN' : {'TYPE' : null,'CODE' : null,'MESSAGE' : null,'LOG_NO' : null,'LOG_MSG_NO' : '000000','MESSAGE_V1' : null,'MESSAGE_V2' : null,'MESSAGE_V3' : null,'MESSAGE_V4' : null}}");
 });
 
