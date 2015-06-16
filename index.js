45ccf7dd7bfdbedda271368a3e3992403b587e84
josuenogueira <josuenogueira@gmail.com>
2015-06-16 16:18:21 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,20 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send("LALALALALALA");
+  response.send("{
+		"export" : {
+			"RETURN" : {
+				"TYPE" : null,
+				"CODE" : null,
+				"MESSAGE" : null,
+				"LOG_NO" : null,
+				"LOG_MSG_NO" : "000000",
+				"MESSAGE_V1" : null,
+				"MESSAGE_V2" : null,
+				"MESSAGE_V3" : null,
+				"MESSAGE_V4" : null
+			}			
+		}");
 });
 
 app.listen(app.get('port'), function() {
