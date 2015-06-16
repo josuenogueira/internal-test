50a2401fe7a58c9e6bd3b9e9a226d4639672b64e
josuenogueira <josuenogueira@gmail.com>
2015-06-16 15:13:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,21 +5,21 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send("'BAPI_SALESORDER_SIMULATE' : {
-		'export' : {
-			'RETURN' : {
-				'TYPE' : null,
-				'CODE' : null,
-				'MESSAGE' : null,
-				'LOG_NO' : null,
-				'LOG_MSG_NO' : '000000',
-				'MESSAGE_V1' : null,
-				'MESSAGE_V2' : null,
-				'MESSAGE_V3' : null,
-				'MESSAGE_V4' : null
-			}			
-		}");
-});
+  response.send("'BAPI_SALESORDER_SIMULATE'"+": {"+
+		"'export'"+" : {"+
+			"'RETURN' : {"+ 
+				"'TYPE' : null,"+
+				"'CODE' : null,"+
+				"'MESSAGE' : null,"+
+				"'LOG_NO' : null,"+
+				"'LOG_MSG_NO' : '000000',"+
+				"'MESSAGE_V1' : null,"+
+				"'MESSAGE_V2' : null,"+
+				"'MESSAGE_V3' : null,"+
+				"'MESSAGE_V4' : null"+
+			"}"+ 
+		"}");
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
