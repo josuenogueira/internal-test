d9ef99d857609c32d25ce8661bee43b409c525e0
josuenogueira <josuenogueira@gmail.com>
2015-06-16 15:15:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,20 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send("'BAPI_SALESORDER_SIMULATE'"+": {"+
-		"'export'"+" : {"+
-			"'RETURN' : {"+ 
-				"'TYPE' : null,"+
-				"'CODE' : null,"+
-				"'MESSAGE' : null,"+
-				"'LOG_NO' : null,"+
-				"'LOG_MSG_NO' : '000000',"+
-				"'MESSAGE_V1' : null,"+
-				"'MESSAGE_V2' : null,"+
-				"'MESSAGE_V3' : null,"+
-				"'MESSAGE_V4' : null"+
-			"}"+ 
-		"}");
+  response.send("LALALALALALA");
 
 
 app.listen(app.get('port'), function() {
